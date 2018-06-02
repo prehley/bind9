c9a27b3c83eaaab6bbd85d7eadc29a543adf3cd9
Peter Rehley <peter_rehley@yahoo.com>
2018-06-02 16:41:05 -0700

    Fix column in perl check.

--- util/entrypoint.sh
+++ util/entrypoint.sh
@@ -34,7 +34,7 @@ if [ -n "$1" -a "$1" == "named" ]; then
         chmod 666 $logfile
     fi
 
-    container_id=$(grep docker /proc/self/cgroup | sort -n | head -n 1 | cut -d: -f3 | cut -d/ -f5)
+    container_id=$(grep docker /proc/self/cgroup | sort -n | head -n 1 | cut -d: -f3 | cut -d/ -f3)
     if perl -e '($id,$name)=@ARGV;$short=substr $id,0,length $name;exit 1 if $name ne $short;exit 0' $container_id $HOSTNAME; then
         echo "You must add the 'docker run' option '--net=host' if you want to provide DNS service to the host network."
     fi
